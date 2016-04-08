d91e451d8b76ed4bed1920dae7ba8247d148c270
webchills <office@webchills.com>
2016-04-08 21:26:29 +0200

    Version 1.8

---
+++ NEUEDATEIEN/includes/languages/english/extra_definitions/products_with_attributes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * extra_definitions/products_with_attributes.php
+ * 
+ * @package functions
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
+ * @version $Id: products_with_attributes.php 2014-08-05 20:02:14Z webchills $
+ */
+define('PWA_STOCK_AVAILABLE', 'Stock Available: ');
+define('PWA_OUT_OF_STOCK ', 'currently not in stock!');
